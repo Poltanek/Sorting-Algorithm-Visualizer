776829827dab386e2642c8d4ae615db7069d3734
Adam Tanweer <113802807+Poltanek@users.noreply.github.com>
2024-07-31 05:55:42 +0100

    1.1.0

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-from tkinter import *
-from tkinter import ttk
-import numpy as np
+from tkinter import * 
+from tkinter import ttk 
+import numpy as np 
 import random 
+import time 
 
 mainWindow = Tk()
 mainWindow.title("Sorting Algorithm Visualizer")
@@ -10,6 +11,7 @@ mainWindow.config(bg="black")
 
 # Variables
 selected_alg = StringVar()
+data = []
 
 def drawData(data):
     MainCanvas.delete("all")
@@ -27,35 +29,43 @@ def drawData(data):
         x1 = (i + 1) * x_width + offset
         y1 = a_height
         MainCanvas.create_rectangle(x0, y0, x1, y1, fill="red")
-        MainCanvas.create_text(x0 + 2, y0, anchor=SW, text=str(height))
+        MainCanvas.create_text(x0 + 2, y0, anchor=SW, text=str(data[i]))
 
+    mainWindow.update_idletasks()
 
 def GenerateArray():
-    print('Alg Selected: ' + selected_alg.get())
-    try:
-        minVal = int(minEntry.get())
-    except:
-        minVal = 1
-    try:
-        maxVal = int(maxEntry.get())
-    except:
-        maxVal = 10
-    try:
-        size = int(SizeEntry.get())
-    except:
-        size = 10
+    global data
+
+    minVal = int(minEntry.get())
+    maxVal = int(maxEntry.get())
+    size = int(SizeEntry.get())
     
     if minVal < 0 : minVal = 0
     if maxVal > 100 : maxVal = 100
     if size > 30 or size < 3 : size = 25
     if minVal > maxVal : minVal, maxVal = maxVal, minVal
 
-
     data = []
     for _ in range(size):
         data.append(random.randrange(minVal, maxVal + 1))
     
     drawData(data)
+
+def bubble_sort(data):
+    for _ in range(len(data) - 1):
+        for j in range(len(data) - 1):
+            if data[j] > data[j + 1]:
+                data[j], data[j + 1] = data[j + 1], data[j]
+            drawData(data)
+            time.sleep(float(SpeedScale.get()))
+    drawData(data)
+
+def StartAlgorithm():
+    global data
+    selected_algorithm = selected_alg.get()
+    if selected_algorithm == "Bubble Sort":
+        bubble_sort(data)
+
 
 # Frames & Canvas 
 # Row 0 
@@ -72,29 +82,25 @@ AlgMenu = ttk.Combobox(UserInterface, textvariable=selected_alg, values=["Bubble
 AlgMenu.grid(row=0, column=1, padx=5, pady=5)
 AlgMenu.current(0)
 
-GenerateBtn = Button(UserInterface, text="Generate Array", command=GenerateArray, bg="black", fg="white")
-GenerateBtn.grid(row=0, column=2, padx=5, pady=5)
+GenerateArrayBtn = Button(UserInterface, text="Generate Array", command=GenerateArray, bg="black", fg="white")
+GenerateArrayBtn.grid(row=0, column=2, padx=5, pady=5)
 
 SpeedScale = Scale(UserInterface, from_=0.1, to=2.0, bg="black", fg="white", length=200, digits=2, resolution=0.2, orient=HORIZONTAL, label="Select Speed [s]")
 SpeedScale.grid(row=0, column=3, padx=5, pady=5)
 
+
 # Row 1
-SizeLbl = Label(UserInterface, text="Size Value", bg="black", fg="white")
-SizeLbl.grid(row=1, column=0, padx=5, pady=5, sticky=W)
-SizeEntry = Entry(UserInterface)
+SizeEntry = Scale(UserInterface, from_=3, to=30, bg="black", fg="white", length=200, resolution=1, orient=HORIZONTAL, label="Select Size")
 SizeEntry.grid(row=1, column=1, padx=5, pady=5, sticky=W)
 
-minLbl = Label(UserInterface, text="Min Value", bg="black", fg="white")
-minLbl.grid(row=1, column=2, padx=5, pady=5, sticky=W)
-minEntry = Entry(UserInterface)
+minEntry = Scale(UserInterface, bg="black", fg="white", from_=0, to=10, length=200, resolution=1, orient=HORIZONTAL, label="Select Min Value")
 minEntry.grid(row=1, column=3, padx=5, pady=5, sticky=W)
 
-maxLbl = Label(UserInterface, text="Max Value", bg="black", fg="white")
-maxLbl.grid(row=1, column=4, padx=5, pady=5, sticky=W)
-maxEntry = Entry(UserInterface)
+maxEntry = Scale(UserInterface, bg="black", fg="white", from_=10, to=100, length=200, resolution=1, orient=HORIZONTAL, label="Select Max Value")
 maxEntry.grid(row=1, column=5, padx=5, pady=5, sticky=W)
 
-
+startBtn = Button(UserInterface, command=bubble_sort, text="Start Sorting", bg="black", fg="white")
+startBtn.grid(row=1, column=6, padx=5, pady=5)
 
 
 mainWindow.mainloop()
