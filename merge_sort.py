058ba7d6d5a2070842abc5ee436f95a4be7bf3c4
Adam Tanweer <113802807+Poltanek@users.noreply.github.com>
2024-08-24 20:29:22 +0100

    Update

--- merge_sort.py
+++ merge_sort.py
@@ -1,22 +1,20 @@
 import time
 
-def merge_sort(arr, drawData, timeTick, stop_check):
+def merge_sort(arr, drawData, timeTick):
     if len(arr) <= 1:
         return arr
     
     mid = len(arr) // 2
-    left_half = merge_sort(arr[:mid], drawData, timeTick, stop_check)
-    right_half = merge_sort(arr[mid:], drawData, timeTick, stop_check)
+    left_half = merge_sort(arr[:mid], drawData, timeTick)
+    right_half = merge_sort(arr[mid:], drawData, timeTick)
     
-    return merge(left_half, right_half, arr, drawData, timeTick, stop_check)
+    return merge(left_half, right_half, arr, drawData, timeTick)
 
-def merge(left, right, arr, drawData, timeTick, stop_check):
+def merge(left, right, arr, drawData, timeTick):
     result = []
     i = j = k = 0
     
     while i < len(left) and j < len(right):
-        if stop_check():
-            return arr
         if left[i] < right[j]:
             result.append(left[i])
             arr[k] = left[i]
@@ -28,23 +26,24 @@ def merge(left, right, arr, drawData, timeTick, stop_check):
         k += 1
         drawData(arr, ['green' if x == k else 'red' for x in range(len(arr))])
         drawData(arr, ['green' if x == k else 'red' for x in range(len(arr))])
+        time.sleep(timeTick)
     
     while i < len(left):
-        if stop_check():
-            return arr
         result.append(left[i])
         arr[k] = left[i]
         i += 1
         k += 1
         drawData(arr, ['green' if x == k else 'red' for x in range(len(arr))])
-    
+        time.sleep(timeTick)
+
     while j < len(right):
-        if stop_check():
-            return arr
         result.append(right[j])
         arr[k] = right[j]
         j += 1
         k += 1
         drawData(arr, ['green' if x == k else 'red' for x in range(len(arr))])
+        time.sleep(timeTick)
+    
+    return result
 
-    return arr
+
